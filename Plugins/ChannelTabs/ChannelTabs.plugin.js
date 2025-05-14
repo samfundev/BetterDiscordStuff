9b8eeb9ad264ed14d9f44c633cfb24a21965e34d
Pharaoh2k <5753365+Pharaoh2k@users.noreply.github.com>
2025-05-14 16:42:12 +0700

    Fixed undefined username
    User name on tabs displays undefined in certain cases. This fixes it.
    example:
    Recipient user data: {
      userId: '241334335884492810', 
      username: 'daddyboard', 
      displayName: undefined, 
      globalName: 'DaddyBoard', 
      isBot: undefined, 
      nickname: undefined
    }
    Final name for user 241334335884492810: undefined
    Final result: undefined (@daddyboard)

--- Plugins/ChannelTabs/ChannelTabs.plugin.js
+++ Plugins/ChannelTabs/ChannelTabs.plugin.js
@@ -1666,10 +1666,10 @@ var getCurrentName = (pathname = location.pathname) => {
 					.map((u) =>
 						!u.display_name && !u.global_name && u.bot
 							? `BOT (@${u.username})`
-							: RelationshipStore.getNickname(u.id) || u.display_name,
+							: RelationshipStore.getNickname(u.id) || u.display_name || u.global_name || u.username,
 					)
 					.join(", ") ||
-				`${channel.rawRecipients[0].display_name} (@${channel.rawRecipients[0].username})`
+				`${channel.rawRecipients[0].display_name || channel.rawRecipients[0].global_name || channel.rawRecipients[0].username} (@${channel.rawRecipients[0].username})`
 			);
 		else return pathname;
 	} else {
