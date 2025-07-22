53ba06d05a616bbc11872381c4619dea583bbf35
samfundev <samdinner@hotmail.com>
2025-07-22 13:11:38 -0400

    fixed: Fixed tabs not updating after popping out a voice call
    Fixes #72

--- src/ChannelTabs/index.jsx
+++ src/ChannelTabs/index.jsx
@@ -3240,12 +3240,6 @@ const TopBar = class TopBar extends React.Component {
 
 		return (
 			<div id="channelTabs-container">
-				{/* Hide ChannelTabs in popouts. Since popouts don't have any the stylesheets from the main window, this must be applied here. */}
-				<style>
-					{`[data-popout-root="true"] #channelTabs-container {
-						display: none;
-					}`}
-				</style>
 				{!this.state.showTabBar ? null : (
 					<TabBar
 						leading={this.props.leading}
@@ -4125,7 +4119,10 @@ html:not(.platform-win) #channelTabs-settingsMenu {
 
 	patchTitleBar(promiseState) {
 		if (promiseState.cancelled) return;
-		Patcher.after(TitleBar, TitleBarKey, (thisObject, _, returnValue) => {
+		Patcher.after(TitleBar, TitleBarKey, (thisObject, [props], returnValue) => {
+			// Only patch if this is the main window.
+			if (props.windowKey !== undefined) return;
+
 			returnValue.props.style = { paddingLeft: 0 };
 			returnValue.props.children = (
 				<TopBar
